e09b21b13fe448d577c123b5d1227c07acf36aa2
Eduardo Damian Dantas <eduardo@damiandantas.com>
2021-10-02 20:52:34 -0400

    Use select instead of radio option
    Parse unit to support hour

--- ui/tabsettings.go
+++ ui/tabsettings.go
@@ -24,16 +24,26 @@ func createSettingsTab(presenter domain.Presenter) *widget.Form {
 	return form
 }
 
-func createIntervalOption(settings *domain.Settings) *widget.RadioGroup {
+func createIntervalOption(settings *domain.Settings) *widget.Select {
 	options := []string{"1 min", "2 min", "5 min", "10 min", "1 hour"}
 
 	onSelected := func(selected string) {
-		durationStr := strings.TrimSuffix(selected, " min")
-		durationInt, _ := strconv.Atoi(durationStr)
-		settings.Interval = time.Duration(durationInt) * time.Minute
+		pieces := strings.Split(selected, " ")
+
+		var unit time.Duration
+		switch pieces[1] {
+		case "min":
+			unit = time.Minute
+		case "hour":
+			unit = time.Hour
+		}
+
+		durationInt, _ := strconv.Atoi(pieces[0])
+
+		settings.Interval = time.Duration(durationInt) * unit
 	}
 
-	return widget.NewRadioGroup(
+	return widget.NewSelect(
 		options,
 		onSelected,
 	)
