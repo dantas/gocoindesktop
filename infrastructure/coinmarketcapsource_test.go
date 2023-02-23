9200e13d942bec5e925a999968d701c4d42f6794
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-02-23 18:55:24 -0500

    Remove garbage leftover from refactoring

--- infrastructure/coinmarketcapsource_test.go
+++ infrastructure/coinmarketcapsource_test.go
@@ -5,23 +5,17 @@ import (
 )
 
 func TestCoinMarketCapSource(t *testing.T) {
-	count := executePrintResultAndReturnCount(t)
-
-	if count == 0 {
-		t.Error("Scrapper did not find any coin")
-	}
-}
-
-func executePrintResultAndReturnCount(t *testing.T) int {
 	coins, err := CoinMarketCapSource()
 
 	if err != nil {
-		t.Errorf("Errors %v\n", err)
+		t.Errorf("Error scrapping coins: %v\n", err)
 	} else {
 		for _, c := range coins {
 			t.Logf("Coin %s : %f\n", c.Name, c.Price)
 		}
 	}
 
-	return len(coins)
+	if len(coins) == 0 {
+		t.Error("Scrapper did not find any coin")
+	}
 }
